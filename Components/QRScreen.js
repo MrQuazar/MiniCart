16b5906f16a8a93457558b3d4ed9abbe3f953575
MrQuazar <aartemsingh03@gmail.com>
2021-09-12 11:46:58 +0530

    QrScreen resized and optimised

--- Components/QRScreen.js
+++ Components/QRScreen.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Text, View, ScrollView, StyleSheet, ImageBackground, Image, TouchableOpacity } from 'react-native';
+import { Dimensions } from 'react-native';
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export default function Order({ navigation }) {
     const [state, setState] = useState({ Name: "Oreo", Price: 30, Shop: "Rizvi Market" })
@@ -20,23 +23,24 @@ export default function Order({ navigation }) {
     return (
         <View style={styles.container}>
             <TouchableOpacity style={styles.ToCart}>
-                <Image source={require('../assets/ToCart.png')} style={{ resizeMode: 'cover', width: '100%', height: '100%' }} />
+                <Image source={require('../assets/ToCart.png')} style={{ resizeMode: 'contain', width: '100%', height: '100%' }} />
             </TouchableOpacity>
-            <Image source={require('../assets/ScanPanel.png')} style={styles.Scanner}>
-            </Image>
+            <View style={styles.Scanner}>
+                <Image source={require('../assets/ScanPanel.png')} style={{ resizeMode: 'contain', width: '100%', height: '100%' }} />
+            </View>
             <TouchableOpacity style={styles.ScanBtn} onPress={() => { shouldScan() }}>
-                <Image source={require('../assets/ScanBtn.png')} style={{ resizeMode: 'cover', width: '100%', height: '100%' }} />
+                <Image source={require('../assets/ScanBtn.png')} style={{ resizeMode: 'contain', width: '100%', height: '100%' }} />
             </TouchableOpacity>
-            <ImageBackground source={require('../assets/InfoPanel.png')} style={styles.InfoDisplay}>
+            <View style={styles.InfoDisplay}>
                 <Text style={styles.CodeStyle1} >Name:{state.Name}</Text>
                 <Text style={styles.CodeStyle2} >Price:{state.Price}</Text>
                 <Text style={styles.CodeStyle3} >Shop:{state.Shop}</Text>
-            </ImageBackground>
+            </View>
             <TouchableOpacity style={styles.AddBtn} onPress={() => { Adder() }}>
-                <Image source={require('../assets/AddBtn.png')} style={{ resizeMode: 'cover', width: '100%', height: '100%' }} />
+                <Image source={require('../assets/AddBtn.png')} style={{ resizeMode: 'contain', width: '100%', height: '100%' }} />
             </TouchableOpacity>
             <TouchableOpacity style={styles.CancelBtn} onPress={() => { Remover() }}>
-                <Image source={require('../assets/CancelBtn.png')} style={{ resizeMode: 'cover', width: '100%', height: '100%' }} />
+                <Image source={require('../assets/CancelBtn.png')} style={{ resizeMode: 'contain', width: '100%', height: '100%' }} />
             </TouchableOpacity>
         </View>
     );
@@ -44,82 +48,76 @@ export default function Order({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         "position": "relative",
-        "width": 414,
-        "height": 896
+        "width": windowWidth,
+        "height": windowHeight,
     },
     Scanner: {
         "position": "absolute",
-        "width": 222,
-        "height": 232,
-        "left": 96,
-        "top": 114
+        "width": 0.536 * windowWidth,
+        "height": 0.258 * windowHeight,
+        "left": 0.231 * windowWidth,
+        "top": 0.126 * windowHeight
     },
     InfoDisplay: {
         "position": "absolute",
-        "width": 300,
-        "height": 267,
-        "left": 57,
-        "top": 485
+        "width": 0.724 * windowWidth,
+        "height": 0.297 * windowHeight,
+        "left": 0.137 * windowWidth,
+        "top": 0.541 * windowHeight,
+        borderWidth: 1,
+        borderRadius: 10,
+        color: "white"
     },
     ToCart: {
         "position": "absolute",
-        "width": 89.42,
-        "height": 56,
-        "left": 309,
-        "top": 38
+        "width": 0.155 * windowWidth,
+        "height": 0.0925 * windowHeight,
+        "left": 0.746 * windowWidth,
+        "top": 0.04 * windowHeight
     },
     ScanBtn: {
         "position": "absolute",
-        "width": 177.65,
-        "height": 64.9,
-        "left": 118.29,
-        "top": 382.99
+        "width": 0.2841 * windowWidth,
+        "height": 0.0724 * windowHeight,
+        "left": 0.34 * windowWidth,
+        "top": 0.427 * windowHeight
     },
     AddBtn: {
         "position": "absolute",
-        "width": 133,
-        "height": 65,
-        "left": 57,
-        "top": 789
+        "width": 0.321 * windowWidth,
+        "height": 0.0725 * windowHeight,
+        "left": 0.137 * windowWidth,
+        "top": 0.88 * windowHeight
     },
     CancelBtn: {
         "position": "absolute",
-        "width": 133,
-        "height": 65,
-        "left": 224,
-        "top": 789
+        "width": 0.321 * windowWidth,
+        "height": 0.0725 * windowHeight,
+        "left": 0.541 * windowWidth,
+        "top": 0.88 * windowHeight
     },
     CodeStyle1: {
-        "position": "absolute",
-        "width": 124,
-        "height": 69,
-        "left": 66,
-        "top": 504,
         "fontStyle": "normal",
         "fontWeight": "normal",
         "fontSize": 20,
-        "lineHeight": 25
+        "lineHeight": 25,
+        paddingLeft: 10,
+        paddingTop: 10,
     },
     CodeStyle2: {
-        "position": "absolute",
-        "width": 108,
-        "height": 69,
-        "left": 66,
-        "top": 581,
         "fontStyle": "normal",
         "fontWeight": "normal",
         "fontSize": 20,
-        "lineHeight": 25
+        "lineHeight": 25,
+        paddingLeft: 10,
+        paddingTop: 10,
     },
     CodeStyle3: {
-        "position": "absolute",
-        "width": 112,
-        "height": 69,
-        "left": 66,
-        "top": 669,
         "fontStyle": "normal",
         "fontWeight": "normal",
         "fontSize": 20,
-        "lineHeight": 25
+        "lineHeight": 25,
+        paddingLeft: 10,
+        paddingTop: 10,
     }
 })
