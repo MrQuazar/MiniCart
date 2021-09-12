d4db32ce9847681ec86f4dd398ca2507125c4ebd
Laik <laikshaikh@eng.rizvi.edu.in>
2021-09-12 15:51:57 +0530

    Splash Screen UI Optimized

--- components/ThankyouPage.js
+++ components/ThankyouPage.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet, Image, Dimensions} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 
-export default function ThankyouPage({navigation}) {
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('screen').height
+
+
+export default function ThankyouPage() {
 
 
     return(
-        <View>
+        <View style={styles.container}>
             <LinearGradient
             start={{x: 1, y: 0}} end={{x: 0, y: 1}}
             colors={['#D49CFF', '#8F00FF' , '#0038FF' , '#102265' ]}
             style={styles.background} />
-
+            <View style = {styles.smilingcart}>
             <Image source={require('../assets/smilecart.png')} 
-                   style = {styles.smilingcart}  />
-            
+                style={{ resizeMode: 'contain', width: '100%', height: '100%' }}     />
+            </View>
+            <View style = {styles.thankyou}>
             <Image source={require('../assets/ThankYou.png')}
-                   style = {styles.thankyou} />
-
+                style={{ resizeMode: 'contain', width: '100%', height: '100%' }}    />
+            </View>
         </View>
 
 
@@ -27,6 +32,13 @@ export default function ThankyouPage({navigation}) {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        position: "relative",
+        width: windowWidth,
+        height: windowHeight
+
+    },
+
     background: {
         position: "relative",
         width: 414,
@@ -39,17 +51,17 @@ const styles = StyleSheet.create({
 
     smilingcart: {
         position: "absolute",
-        width: 335,
-        height: 195,
-        top: 215
+        width: 0.852 * windowWidth,
+        height: 0.228* windowHeight,
+        top: 0.224 * windowHeight,
     },
 
     thankyou: {
         position: "absolute",
-        width: 320,
-        height: 95,
-        left: 30,
-        top: 500
+        width: 0.772 * windowWidth,
+        height: 0.112 * windowHeight,
+        left: 0.113 * windowWidth,
+        top: 0.558 * windowHeight
       }
     
 });
